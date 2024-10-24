f79a7f6b12d74bd97d57787748c0fafd76f6b43b
saividya024 <chsaividya180305@gmail.com>
2024-10-24 14:03:11 +0530

    Add files via upload

---
+++ loop statement.js
@@ -0,0 +1,51 @@
+for(var a=1;a<3;a++){
+    console.log(a)
+}
+//o/p:1,2
+for(var a=10;a<=15;a++){
+    console.log(a)
+}
+//o/p:10,11,12,13,14,15
+for(var a=11;a>6;a--){
+    console.log(a)
+}
+//o/p:11,10,9,8,7
+for(var a=7;a>=2;a--){
+ console.log(a)
+}
+//o/p:7,6,5,4,3,2
+var a=1;
+while(a<7){
+    console.log(a)
+    a++
+}
+//o/p:1,2,3,4,5,6
+var a=1;
+while(a<=7){
+    console.log(a)
+    a++}
+//o/p:1,2,3,4,5,6,7
+var a=11
+while(a>=7){
+    console.log(a)
+ a--
+}
+//o/p:11,10,9,8,7
+for(a=1;a<=5;a++){
+}
+console.log(a)
+//o/p:1,2,3,4,5,6
+var a=1;
+do{
+    a++;
+    console.log(a)
+}
+while(a<=5)
+    
+    //o/p:2,3,4,5,6
+var a=11;
+do{
+    a--;
+    console.log(a)
+} while(a>=8)
+    //o/p:11,10,9,8,7
